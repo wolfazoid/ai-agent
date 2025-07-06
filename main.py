31fb511d685d031cad6484fadb341a87adf6c07f
wolfazoid <wolfahoward@gmail.com>
2025-07-06 18:41:40 -0500

    Set up function schema for AI agent to read from, added a system prompt to enable AI to list files, read file content, execute python and write or overwrite files. Did initial set up of 'call_function' method within main.py TODO: Finish 'call_function' method so the ai agent can run python scripts.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions.functions_schema import *
 
 def main():
     load_dotenv()
@@ -32,9 +33,33 @@ def main():
     
 
 def generate_content(client, messages, verbose):
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+    - Read file contents
+    - Execute Python files with optional arguments
+    - Write or overwrite files
+    
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+    
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+            schema_get_file_content,
+            schema_run_python_file,
+            schema_write_file
+        ]
+    )
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        )
     )
     
     if verbose:
@@ -43,6 +68,9 @@ def generate_content(client, messages, verbose):
 
 
     print(f"Response:\n{response.text}")
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 
 if __name__ == "__main__":
